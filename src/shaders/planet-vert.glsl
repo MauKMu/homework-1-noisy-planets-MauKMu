c7d1a22c24ffc47cd488cbb546f56a73baf2f4a5
MauKMu <maukmu@users.noreply.github.com>
2018-01-25 13:05:57 -0500

    Tweak erosion

--- src/shaders/planet-vert.glsl
+++ src/shaders/planet-vert.glsl
@@ -390,13 +390,13 @@ void main()
     xzAngle = f;
     //float erosion = f;// getFBM(worley.closest0 + vec3(1.1, 1.7, 3.1));
     float erosion = f * smoothstep(0.33, 1.0, xzAngle);
-    t = pow(t, mix(1.0, 3.0, erosion)) * mix(0.8, 1.87, erosion) + mix(0.0, 0.2, erosion);
+    t = pow(t, mix(0.77, 3.0, erosion)) * mix(0.8, 3.27, erosion) + mix(0.0, 0.0, erosion);
     t = 0.5 + 1.5 * t;
     //fs_Col.xyz = vec3(t - 1.0);
     //t = pow(t, mix(1.0, 1.44, cos(time * 10.0) * 0.5 + 0.5));
     vec4 naturePos = u_Model * (vec4(t, t, t, 1.0) * vs_Pos);   // Temporarily store the transformed vertex positions for use below
     vec4 bldgPos = u_Model * (bldgDisp + vs_Pos);   // Temporarily store the transformed vertex positions for use below
-    vec4 modelposition = mix(bldgPos, naturePos, smoothstep(0.1667, 0.33, xzAngle));
+    vec4 modelposition = mix(bldgPos, naturePos,  smoothstep(0.1667, 0.33, xzAngle));
     fs_Pos = modelposition.xyz;
 
     //vec3 localNor = fbmNormal;
